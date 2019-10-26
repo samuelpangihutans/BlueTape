1316a77f28737945544321e8e0807b6c4b3717c8
samuel sihombing <7316054@student.unpar.ac.id>
2019-10-26 09:39:34 +0700

    configurasi awal , remove method email_model.php

--- www/application/config/auth-test.php
+++ www/application/config/auth-test.php
@@ -3,17 +3,16 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $config['domain'] = 'http://localhost';
-$config['google-clientid'] = '306988856488-pfc4b6e9nogigaifjldnmh2hlac7sgmh.apps.googleusercontent.com';
-$config['google-clientsecret'] = 'IhJhqNU-FuyZNMbfsLnmULj1';
+$config['google-clientid'] = '466653343196-620lga9usl4lsmr99o9k1kljrd94ecfj.apps.googleusercontent.com';
+$config['google-clientsecret'] = 'a8yaddxuxBLO9JR8to9vkxyT';
 $config['google-redirecturi'] = $config['domain'] . '/auth/oauth2callback';
 
 $config['email-config'] = Array(
     'protocol' => 'smtp',
-    'smtp_host' => 'smtp.googlemail.com',
-    'smtp_port' => 587,
+    'smtp_host' => 'ssl://smtp.googlemail.com',
+    'smtp_port' => 465,
     'smtp_user' => 'rootbluetape@gmail.com', //masukan email google 
     'smtp_pass' => 'Rootbluetape123', //password email  // di config bikin 2 AUTH 1 YANG SALAH
     'mailtype' => 'html',
-    'charset' => 'iso-8859-1',
-    'smtp_crypto'=> 'tls'
+    'charset' => 'iso-8859-1'
 );
